5ab439392f797c37bd553dc09c1a916b11d9a19a
정유진 <jane320@naver.com>
2024-12-05 15:24:25 +0900

    style: 독서 달력에 책 표지 이미지 추가 (#27)

--- app/mypage/_components/MyReadingCalendar.tsx
+++ app/mypage/_components/MyReadingCalendar.tsx
@@ -3,8 +3,45 @@
 import Calendar from 'react-calendar';
 import './CustomReadingCalendar.css';
 import { formatDate } from 'date-fns';
+import Image from 'next/image';
 
 export default function MyReadingCalendar() {
+  const books = [
+    {
+      date: '2024-12-05',
+      bookCover:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjG17iALTrgmpEhD9eNsSfFsJNVMIgq5AgRg&s',
+      title: 'Book 1',
+    },
+    {
+      date: '2024-12-10',
+      bookCover:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjG17iALTrgmpEhD9eNsSfFsJNVMIgq5AgRg&s',
+      title: 'Book 2',
+    },
+    {
+      date: '2024-12-18',
+      bookCover:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjG17iALTrgmpEhD9eNsSfFsJNVMIgq5AgRg&s',
+      title: 'Book 3',
+    },
+  ];
+  const getBookCover = ({ date }: { date: Date }) => {
+    const formattedDate = date.toISOString().split('T')[0];
+    const data = books.find((b) => b.date === formattedDate);
+
+    return data ? (
+      <div className='absolute top-1/4 left-1/4'>
+        <Image
+          src={data.bookCover}
+          className='z-0'
+          alt={data.title}
+          width='50'
+          height='75'
+        />
+      </div>
+    ) : null;
+  };
   return (
     <Calendar
       locale='en'
@@ -13,6 +50,7 @@ export default function MyReadingCalendar() {
       prev2Label={null}
       next2Label={null}
       showNeighboringMonth={false}
+      tileContent={getBookCover}
     />
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;

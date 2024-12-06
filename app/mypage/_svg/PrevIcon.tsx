7ffdfb95b535b3e73dac945b3968c15fbc36bf07
정유진 <jane320@naver.com>
2024-12-06 13:22:14 +0900

    style: 독서 달력 스타일 수정 (#27)

---
+++ app/mypage/_svg/PrevIcon.tsx
@@ -0,0 +1,19 @@
+export default function PrevIcon() {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M15.0001 17.9998L9.00012 11.9998L15.0001 5.99976'
+        stroke='black'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}

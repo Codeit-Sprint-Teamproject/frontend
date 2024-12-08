719ff7339ca89aa5c9de41c2c8c5fc9d469142bb
정유진 <jane320@naver.com>
2024-12-09 00:26:02 +0900

    style: 마이 페이지 레이아웃 너비 및 패딩 값 고정 (#33)

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -3,11 +3,11 @@ import Profile from './_components/Profile';
 
 export default function MyPage() {
   return (
-    <>
+    <div className='w-[750px] p-5'>
       <h3 className='text-[32px] font-semibold my-2'>마이페이지</h3>
       <Profile />
       <h4 className='text-xl font-bold mb-5'>나의 독서 달력</h4>
       <MyReadingCalendar />
-    </>
+    </div>
   );
 }

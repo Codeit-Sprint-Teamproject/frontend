23adb2795b6c22f36e648f3c4a5820950b10716c
정유진 <jane320@naver.com>
2024-12-09 00:20:48 +0900

    feat: 마이페이지 나의 모임 탭 컴포넌트 추가 (#33)

--- app/mypage/meetings/page.tsx
+++ app/mypage/meetings/page.tsx
@@ -1,3 +1,13 @@
+import Tab from './_components/Tab';
+import TabProvider from './_components/TabContext';
+
 export default function MyMeetingPage() {
-  return <div>참여중인 모임 페이지</div>;
+  return (
+    <div>
+      <h3 className='text-xl font-bold mb-[15px]'>나의 모임</h3>
+      <TabProvider>
+        <Tab />
+      </TabProvider>
+    </div>
+  );
 }

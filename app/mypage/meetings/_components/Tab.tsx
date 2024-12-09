bc5111564e101e44034f21582021a6bfe68d3a14
정유진 <jane320@naver.com>
2024-12-09 11:20:02 +0900

    refactor: 나의 모임 탭 리팩토링 (#33)

--- app/mypage/meetings/_components/Tab.tsx
+++ app/mypage/meetings/_components/Tab.tsx
@@ -4,45 +4,28 @@ import { useTabContext } from './TabContext';
 
 export default function Tab() {
   const { tab, setTab } = useTabContext();
+  console.log('tab', tab);
+  const tabs = [
+    { value: 'active', label: '참여 중인 모임', count: 3 },
+    { value: 'completed', label: '완료한 모임', count: 3 },
+    { value: 'created', label: '내가 만든 모임', count: 3 },
+    { value: 'bookmark', label: '찜한 모임', count: 3 },
+  ] as const;
 
   return (
     <div className='flex gap-[34px] text-xl border-b h-12'>
-      <div
-        className={`cursor-pointer ${tab === 'active' ? 'border-b-4 border-black' : ''}`}
-        onClick={() => setTab('active')}
-      >
-        참여 중인 모임
-        <span className='ml-[3px] px-2 bg-black text-white rounded-[30px] text-xl'>
-          3
-        </span>
-      </div>
-      <div
-        className={`cursor-pointer ${tab === 'completed' ? 'border-b-4 border-black' : ''}`}
-        onClick={() => setTab('completed')}
-      >
-        완료한 모임
-        <span className='ml-[3px] px-2 bg-black text-white rounded-[30px] text-xl'>
-          3
-        </span>
-      </div>
-      <div
-        className={`cursor-pointer ${tab === 'created' ? 'border-b-4 border-black' : ''}`}
-        onClick={() => setTab('created')}
-      >
-        내가 만든 모임
-        <span className='ml-[3px] px-2 bg-black text-white rounded-[30px] text-xl'>
-          3
-        </span>
-      </div>
-      <div
-        className={`cursor-pointer ${tab === 'bookmark' ? 'border-b-4 border-black' : ''}`}
-        onClick={() => setTab('bookmark')}
-      >
-        찜한 모임
-        <span className='ml-[3px] px-2 bg-black text-white rounded-[30px] text-xl'>
-          3
-        </span>
-      </div>
+      {tabs.map(({ value, label, count }) => (
+        <div
+          key={value}
+          className={`cursor-pointer ${tab === value ? 'border-b-4 border-black' : ''}`}
+          onClick={() => setTab(value)}
+        >
+          {label}
+          <span className='ml-[3px] px-2 bg-black text-white rounded-[30px] text-xl'>
+            {count}
+          </span>
+        </div>
+      ))}
     </div>
   );
 }

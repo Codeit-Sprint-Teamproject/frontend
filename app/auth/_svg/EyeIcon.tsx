2ee5c398e6e4a6f3981e042ed79a4d623f616fda
정유진 <jane320@naver.com>
2024-12-14 23:05:32 +0900

    feat: 회원가입 시 약관 동의 기능 추가 (#50)

--- app/auth/_svg/EyeIcon.tsx
+++ app/auth/_svg/EyeIcon.tsx
@@ -6,13 +6,22 @@ const EyeIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
       {...props}
       viewBox='0 0 24 24'
       fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
+      xmlns='http://www.w3.org/2000/svg'
     >
-      <path d='M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z' />
-      <circle cx='12' cy='12' r='3' />
+      <path
+        d='M1 12C1 12 5 5 12 5C19 5 23 12 23 12C23 12 19 19 12 19C5 19 1 12 1 12Z'
+        stroke='#A9A9A9'
+        stroke-width='1.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+      <path
+        d='M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z'
+        fill='#A9A9A9'
+        stroke='#A9A9A9'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
     </svg>
   );
 };

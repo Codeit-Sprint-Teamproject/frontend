e9a0c70b31e13a75732c62fb08c716dcfd23dcec
heee1 <p1xell@kakao.com>
2024-12-17 09:39:00 +0900

    chore: api를 통해 받는 이미지 도메인 config파일에 명시 (#59)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'dev-codeit-4.s3.ap-northeast-2.amazonaws.com',
+      'image.yes24.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- public/ChevronDown.tsx
+++ public/ChevronDown.tsx
@@ -7,7 +7,7 @@ const ChevronDown = ({width, height}:ChevronDownProps) => {
   return (
     <>
       <svg width={`${width}px`} height={`${height}px`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M6 9L12 15L18 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M6 9L12 15L18 9" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
       </svg>
     </>
   );

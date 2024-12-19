1eb2e1a4e0e14fe951ada1e9ce722f6a0fd110b1
heee1 <p1xell@kakao.com>
2024-12-19 18:21:03 +0900

    feat: 책 정보 컴포넌트 일부수정 (#66)

--- app/meeting-detail/[meetingsId]/_components/BookInfo.tsx
+++ app/meeting-detail/[meetingsId]/_components/BookInfo.tsx
@@ -1,43 +1,44 @@
-/**
- * bookTitle: string;
- * author: string;
- * publisher: string;
- * publishDate: string;
- * star: Number;
- */
+import Image from 'next/image';
 
 interface BookInfoProps {
-  bookTitle: string;
-  author: string;
-  publisher: string;
-  publishDate: string;
-  star: number;
+  bookImage?: string;
+  bookTitle?: string;
+  author?: string;
+  publisher?: string;
+  publishDate?: string;
+  star?: number;
 }
 
 const BookInfo = ({
-  bookTitle,
-  author,
-  publisher,
-  publishDate,
-  star,
+  bookImage,
+  bookTitle = '책 이름',
+  author = '저자',
+  publisher = '출판사',
+  publishDate = '출판일',
+  star = 10,
 }: BookInfoProps) => {
   return (
     <>
       <div className='mt-[9px] py-[16px] px-[10px] flex flex-row border-[1px] border-[rgba(0, 0, 0, 0.10)]'>
-        <div className='w-[87px] h-[132px] flex justify-center items-center bg-orange-300'>
-          이미지 영역
+        <div className='w-[87px] h-[132px] flex justify-center items-center relative'>
+          <Image
+            src={bookImage}
+            alt='book-image'
+            layout='fill'
+            objectFit='cover'
+          />
         </div>
         <div className='ml-[18px]'>
           <span className='text-xl font-bold'>{bookTitle}</span>
           <div className='grid grid-cols-[1fr_2fr] mt-[9px]'>
             <span>저자</span>
-            <span>{author}</span>
+            <span className='ml-4'>{author}</span>
             <span>출판</span>
-            <span>{publisher}</span>
+            <span className='ml-4'>{publisher}</span>
             <span>발행일</span>
-            <span>{publishDate}</span>
+            <span className='ml-4'>{publishDate}</span>
             <span>평점</span>
-            <span>☆☆☆☆☆ {`${star}`}</span>
+            <span className='ml-4'>☆☆☆☆☆ {`${star}`}</span>
           </div>
         </div>
       </div>

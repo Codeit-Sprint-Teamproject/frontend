a4501d3adcbf4c37d59e0f73dc7dc914b17278d1
heee1 <p1xell@kakao.com>
2024-12-23 11:14:48 +0900

    feat: tanstackquery를 사용하는 코드로 변경하기위해 파일구조변경 (#66)

--- app/meeting-detail/[meetingsId]/page.tsx
+++ app/meeting-detail/[meetingsId]/page.tsx
@@ -1,20 +1,19 @@
 'use client';
 
-import MeetingDetailLeft from './_components/MeetingDetailLeft';
-import MeetingDetailRight from './_components/MeetingDetailRight';
-import MeetingDetailTabs from './_components/MeetingDetailTabs';
-import { MeetingProvider } from './_lib/MeetingDetailContext';
+import MeetingDetails from './_components/MeetingsDetails';
 
-export default function MeetingDetailPage() {
-  return (
-    <MeetingProvider>
-      <div className='flex flex-col items-center justify-start mt-[131px] w-[1060px] h-[2000px] mb-20 mx-auto '>
-        <div className='w-full h-[670px] flex flex-row justify-between'>
-          <MeetingDetailLeft />
-          <MeetingDetailRight />
-        </div>
-        <MeetingDetailTabs />
-      </div>
-    </MeetingProvider>
-  );
+interface MeetingPageProps {
+  params: {
+    meetingsId: string;
+  };
+}
+
+export default function MeetingDetailPage({ params }: MeetingPageProps) {
+  const gatheringId = Number(params.meetingsId);
+
+  if (isNaN(gatheringId)) {
+    return <div>Wrong Meeting ID</div>;
+  }
+
+  return <MeetingDetails gatheringId={gatheringId} />;
 }

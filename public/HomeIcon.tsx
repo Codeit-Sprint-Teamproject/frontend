ef9826a0fa8babff2ba03ca51633c6eabe8b2b40
heee1 <p1xell@kakao.com>
2024-12-24 16:38:29 +0900

    style: 페이지 로케이터 구현 (#82)

---
+++ public/HomeIcon.tsx
@@ -0,0 +1,8 @@
+export default function HomeIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+      <svg {...props} viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.75 5.25033L7 1.16699L12.25 5.25033V11.667C12.25 11.9764 12.1271 12.2732 11.9083 12.492C11.6895 12.7107 11.3928 12.8337 11.0833 12.8337H2.91667C2.60725 12.8337 2.3105 12.7107 2.09171 12.492C1.87292 12.2732 1.75 11.9764 1.75 11.667V5.25033Z" stroke="black" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M5.25 12.8333V7H8.75V12.8333" stroke="black" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+  );
+}

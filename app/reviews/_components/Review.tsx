0c500cf30c411c06e7f4fbd02d8754d772e13bf1
정유진 <jane320@naver.com>
2024-12-24 10:41:46 +0900

    style: 리뷰 UI 수정 (#72)

--- app/reviews/_components/Review.tsx
+++ app/reviews/_components/Review.tsx
@@ -26,30 +26,42 @@ export default function Review({ review }: { review: BookReview }) {
   };
   return (
     <div className='border w-[520px]'>
-      <h3 className='p-2'>{title}</h3>
-      <div className='flex gap-2.5 p-3 border-y'>
+      <div className='flex items-center  justify-between px-5'>
+        <h3 className='text-xl font-bold py-3'>{title}</h3>
+        <span className='text-sm text-customGrey-500'>이 책을 추천해요</span>
+      </div>
+      <div className='w-11/12 border border-customGrey-100 mx-auto'></div>
+      <div className='flex gap-4 px-5 py-3'>
         <Image
           src={bookImage}
-          width={130}
-          height={195}
-          className='w-[130px] h-[195px]'
+          width={132}
+          height={198}
+          className='w-[132px] h-[198px]'
           alt='책 표지'
         />
-        <p>{review.content}</p>
+        <p className='w-full'>{review.content}</p>
       </div>
+      <div className='w-11/12 border border-customGrey-100 mx-auto'></div>
       <div className='flex justify-between items-center px-5 py-3'>
         <div className='flex items-center gap-2.5'>
           <div className='w-10 h-10 bg-[#D9D9D9] rounded-full'></div>
           <span>{userName}</span>
-          <p>{createTime}</p>
+          {/* TODO (유진) 몇 시간 전 또는 며칠 전으로 수정할 예정 */}
+          <p className='text-customGrey-300'>{createTime}</p>
         </div>
-        <div className='flex gap-1'>
-          <button onClick={handleLike}>
-            <LikeIcon className={`w-5 h-5 ${userLikeCk ? 'fill-black' : ''}`} />
-          </button>
-          <span className='ml-1'>{likes}</span>
-          <CommentIcon className='w-5 h-5' />
-          <p className='ml-1'>{commentCnt || 0}</p>
+        <div className='flex gap-5'>
+          <div className='flex gap-1'>
+            <button onClick={handleLike}>
+              <LikeIcon
+                className={`w-5 h-5 ${userLikeCk ? 'fill-black' : ''}`}
+              />
+            </button>
+            <span>{likes}</span>
+          </div>
+          <div className='flex items-center gap-1'>
+            <CommentIcon className='w-5 h-5' />
+            <span>{commentCnt || 0}</span>
+          </div>
         </div>
       </div>
     </div>

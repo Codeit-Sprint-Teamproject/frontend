0e450ce6b7d2b77c9f1622c93609e5bf68baf851
heee1 <p1xell@kakao.com>
2024-12-24 10:02:42 +0900

    feat: 모임상세페이지 > 모임소개 탭 API연결 (#66)
    - 모임소개 탭 API연결 완료
    - 완료되지 못한 부분
      - 전체 페이지수 ( api 데이터 없음 -> 요청 드림 )
      - 모임 기간 ( api 데이터 없음 -> 요청 드림)
    - 추가로 해야할 작업
      - 연결되지 못한 api연결

--- app/meeting-detail/[meetingsId]/_lib/meetingDetail.ts
+++ app/meeting-detail/[meetingsId]/_lib/meetingDetail.ts
@@ -14,3 +14,16 @@ export const getMeetingDetails = async (gatheringId: number) => {
 
   return data;
 };
+
+export const getMeetingInfo = async (gatheringId: number) => {
+  const endpoint = `/api/gatheringSearch/${gatheringId}/introduce`;
+  const method = 'GET';
+
+  const data = await fetchAPIServer(endpoint, method);
+
+  if (data?.error) {
+    throw new Error(data.error.message || 'Failed to fetch meeting detail');
+  }
+
+  return data;
+};

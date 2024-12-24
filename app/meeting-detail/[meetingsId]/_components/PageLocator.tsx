5c66848529bfee09ff5d08da6c68194e7ac25bcf
heee1 <p1xell@kakao.com>
2024-12-24 16:38:29 +0900

    style: 페이지 로케이터 구현 (#82)

---
+++ app/meeting-detail/[meetingsId]/_components/PageLocator.tsx
@@ -0,0 +1,23 @@
+import ChevronRight from '@/public/ChevronRight';
+import HomeIcon from '@/public/HomeIcon';
+
+interface PageLocatorProps {
+  pagePath: string[];
+  currentPage: string;
+}
+
+const PageLocator = ({ pagePath, currentPage }: PageLocatorProps) => {
+  return (
+    <div className='flex flex-row justify-start items-center text-sm font-medium'>
+      <HomeIcon width={14} height={14} className='mr-2' />
+      {pagePath.map((item, idx) => (
+        <div key={idx} className='flex flex-row justify-start items-center'>
+          <span>{item}</span>
+          <ChevronRight width={14} height={14} />
+        </div>
+      ))}
+      <span>{currentPage}</span>
+    </div>
+  );
+};
+export default PageLocator;

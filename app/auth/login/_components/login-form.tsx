edfc210e5691e5e375ba76606e57cad359abb1a8
suk3952111 <suk3952111@naver.com>
2024-12-26 15:53:04 +0900

    refactor :  전달받은 유저정보 store에 저장  (#90)

--- app/auth/login/_components/login-form.tsx
+++ app/auth/login/_components/login-form.tsx
@@ -1,28 +1,54 @@
 'use client';
 
-import { useFormState } from 'react-dom';
+import { useState } from 'react';
 import { FormButton } from '../../_components/form-button';
 import { userLogIn } from '../_lib/login';
 import { LoginFormInput } from './login-form-input';
+import useUserStore from '@/store/userStore';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export const LoginForm = () => {
-  const initialState = { message: '', errors: {} };
-  const [state, dispatch] = useFormState(userLogIn, initialState);
+  const { setUser } = useUserStore();
+  const [state, setState] = useState({ message: '', errors: {} });
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    try {
+      const result = await userLogIn(formData);
+      console.log(result);
+
+      if (result.errors) {
+        setState({ message: result.message, errors: result.errors });
+      } else if (result.user) {
+        setUser(result.user);
+        router.push('/');
+      } else if (result.message) {
+        setState({ message: result.message, errors: {} });
+      }
+    } catch (error) {
+      console.error('Unexpected error:', error);
+      setState({ message: '예상치 못한 오류가 발생했습니다.', errors: {} });
+    }
+  };
+
   return (
-    <form action={dispatch} className='max-w-[380px]'>
+    <form onSubmit={handleSubmit} className='max-w-[380px]'>
       <div className='flex flex-col space-y-2'>
-        <LoginFormInput errors={state?.errors} />
+        <LoginFormInput errors={state.errors} />
         <div className='h-6'>
-          {state?.message && (
+          {state.message && (
             <p className='text-sm text-error'>{state.message}</p>
           )}
         </div>
       </div>
       <div className='flex flex-col mt-[32px] gap-5'>
         <FormButton
-          className='w-full h-12 p-2.5 text-base font-bold bg-black text-white rounded'
           disabled={false}
+          className='w-full h-12 p-2.5 text-base font-bold bg-black text-white rounded'
         >
           로그인
         </FormButton>

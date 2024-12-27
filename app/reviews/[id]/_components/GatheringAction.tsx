caa48dd8770429d2195f19d73f44ae3dda7cf8e0
정유진 <jane320@naver.com>
2024-12-27 10:22:36 +0900

    fix: 모임 안내 컴포넌트 수정 (#89)

--- app/reviews/[id]/_components/GatheringAction.tsx
+++ app/reviews/[id]/_components/GatheringAction.tsx
@@ -1,11 +1,13 @@
 export default function GatheringAction({ status }: { status: boolean }) {
   if (status) {
-    <div className='flex justify-between items-center w-full h-14 bg-customGrey-50 p-2 rounded-[2px]'>
-      <p className='text-customGrey-800'>현재 모집 중인 모임이 있어요.</p>
-      <button className='px-3 py-2 bg-customGreen-50 text-customGreen-600 rounded-sm'>
-        보러 가기
-      </button>
-    </div>;
+    return (
+      <div className='flex justify-between items-center w-full h-14 bg-customGrey-50 p-2 rounded-[2px]'>
+        <p className='text-customGrey-800'>현재 모집 중인 모임이 있어요.</p>
+        <button className='px-3 py-2 bg-customGreen-50 text-customGreen-600 rounded-sm'>
+          보러 가기
+        </button>
+      </div>
+    );
   }
   return (
     <div className='flex justify-between items-center w-full h-14 bg-customGrey-50 p-2 rounded-[2px]'>

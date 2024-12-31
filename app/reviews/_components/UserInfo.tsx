a36186846f802257106227620069c4e452c5bbfd
정유진 <jane320@naver.com>
2025-01-01 02:23:01 +0900

    feat: 리뷰 메인 페이지 사용자 정보 표시 (#114)

--- app/reviews/_components/UserInfo.tsx
+++ app/reviews/_components/UserInfo.tsx
@@ -1,14 +1,30 @@
+'use client';
+
+import Avatar from '@/components/common/icons/Avatar';
 import SlideNextIcon from '@/components/common/icons/SlideNextIcon';
+import useUserStore from '@/store/userStore';
+import Image from 'next/image';
 
 export default function UserInfo() {
+  const { user } = useUserStore();
   return (
     <div className='flex items-center gap-4 p-5'>
-      <div className='w-[60px] h-[60px] bg-[#A0A0A0] rounded-full'></div>
+      {user?.profile ? (
+        <Image
+          src={user?.profile}
+          width={56}
+          height={56}
+          className='rounded-full'
+          alt='프로필'
+        />
+      ) : (
+        <Avatar className='w-14 h-14' />
+      )}
       <div>
-        <p className='font-bold'>테스트</p>
+        <p className='text-lg font-bold'>{user?.name}</p>
         <div className='flex items-center'>
-          <p className='text-customGrey-500'>작성한 독서 리뷰</p>
-          <SlideNextIcon className='w-5 h-5' />
+          <p className='text-sm text-customGrey-500'>작성한 독서 리뷰</p>
+          <SlideNextIcon className='w-5 h-5 stroke-customGrey-500' />
         </div>
       </div>
     </div>

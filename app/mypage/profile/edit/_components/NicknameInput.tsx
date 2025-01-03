3aa0fbf992b8759fda1c08c70be82f18a8ccdccb
정유진 <jane320@naver.com>
2025-01-04 00:39:38 +0900

    style: 프로필 닉네임 수정 UI 수정 (#121)

--- app/mypage/profile/edit/_components/NicknameInput.tsx
+++ app/mypage/profile/edit/_components/NicknameInput.tsx
@@ -25,39 +25,49 @@ export default function NicknameInput() {
     });
   };
   return (
-    <div className='flex flex-col gap-2'>
-      <p className='text-customGrey-800'>닉네임</p>
-      <div className='flex gap-3'>
-        <input
-          type='text'
-          value={isEdit ? nickname : ''}
-          className='flex-1 border px-2.5 py-1.5 rounded-sm border-customGrey-200'
-          placeholder={user?.name}
-          onChange={(e) => setNickname(e.target.value)}
-          disabled={!isEdit}
-        />
-        {!isEdit && (
-          <button
-            className='h-11 p-2.5 bg-customGrey-800 text-white rounded-sm'
-            onClick={() => setIsEdit(true)}
-          >
-            변경하기
-          </button>
+    <div className={`rounded-sm ${isEdit ? 'bg-customGrey-50 px-5 py-4' : ''}`}>
+      {isEdit && (
+        <p className='font-bold text-customGrey-800 pb-5'>닉네임 변경</p>
+      )}
+      <div className='flex flex-col gap-1.5'>
+        <p className='text-customGrey-800'>닉네임</p>
+        <div className='flex gap-3'>
+          <input
+            type='text'
+            value={nickname}
+            className={`flex-1 border px-2.5 py-1.5 rounded-sm border-customGrey-200 ${!isEdit ? 'text-customGrey-300' : ''}`}
+            placeholder={
+              isEdit ? '변경하실 닉네임을 입력해주세요.' : user?.name
+            }
+            onChange={(e) => setNickname(e.target.value)}
+            disabled={!isEdit}
+          />
+          {!isEdit && (
+            <button
+              className='h-11 p-2.5 bg-customGrey-800 text-white rounded-sm'
+              onClick={() => setIsEdit(true)}
+            >
+              변경하기
+            </button>
+          )}
+        </div>
+        {isEdit && (
+          <div className='flex justify-end mt-[14px]'>
+            <button
+              className='text-sm text-customGrey-800 p-2.5'
+              onClick={() => setIsEdit(false)}
+            >
+              취소
+            </button>
+            <button
+              className='w-[74px] h-10 text-sm p-2.5 bg-black text-white rounded-sm'
+              onClick={handleChangeNickname}
+            >
+              저장
+            </button>
+          </div>
         )}
       </div>
-      {isEdit && (
-        <div className='flex justify-end'>
-          <button className='p-2.5' onClick={() => setIsEdit(false)}>
-            취소
-          </button>
-          <button
-            className='w-[74px] p-2.5 bg-black text-white rounded-sm'
-            onClick={handleChangeNickname}
-          >
-            저장
-          </button>
-        </div>
-      )}
     </div>
   );
 }

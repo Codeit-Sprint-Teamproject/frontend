a7c7c6a126d708ff83687e1a3433bb6a40401dae
heee1 <p1xell@kakao.com>
2025-01-05 06:45:05 +0900

    feat: 헤더, 검색 UI 업데이터 마무리, 기능구현 완료 (#116)

--- app/_components/MainPageBody.tsx
+++ app/_components/MainPageBody.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from 'react';
 import Calendar from 'react-calendar';
+import BookRankIcon from '../_svg/BookRankIcon';
 import { IFilterState, IMeeting, IPopularBooks } from '../types';
 import CustomDropdown from './CustomDropdown';
 import { getPopularBooks } from './_lib/getPopularBooks';
@@ -9,9 +10,9 @@ import { useMeetingsInfiniteQuery } from './_lib/useMeetingsInfiniteQuery';
 import SearchIcon from '@/app/_svg/SearchIcon';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Progress } from '@/components/ui/progress';
 import { Switch } from '@/components/ui/switch';
 import BookIcon from '@/public/BookIcon';
-import CalendarDotIcon from '@/public/CalendarDotIcon';
 import CalendarIcon from '@/public/CalendarIcon';
 import ChevronDownIcon from '@/public/ChevronDownIcon';
 import RotateCwIcon from '@/public/RotateCwIcon';
@@ -19,6 +20,7 @@ import UsersIcon from '@/public/UsersIcon';
 import { format } from 'date-fns';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function MainPageBody() {
   const [meetingsData, setMeetingsData] = useState<IMeeting[]>([]);
@@ -28,9 +30,10 @@ export default function MainPageBody() {
     recruitingOnly: true,
     targetTime: null,
   });
-  const [searchBarHasText, setSearchBarHasText] = useState(false);
+  const [searchBarHasText, setSearchBarHasText] = useState<string>('');
   const { data, isLoading, isError, error } = useMeetingsInfiniteQuery();
   const today = new Date();
+  const router = useRouter();
 
   useEffect(() => {
     if (data?.pages && data.pages.length > 0) {
@@ -107,9 +110,15 @@ export default function MainPageBody() {
               </div>
             )}
             <Input
-              className='w-full h-full px-4 border-[2px] border-customGreen-500 font-medium text-[18px]'
-              onChange={(e) => setSearchBarHasText(!!e.target.value)}
+              className='w-full h-full px-4 border-[2px] border-customGreen-500 font-medium text-[18px] shadow-lg'
+              onChange={(e) => setSearchBarHasText(e.target.value)}
+              value={searchBarHasText}
               placeholder='읽고 싶은 책의 모임을 찾아보세요'
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  router.push(`/search/${searchBarHasText}`);
+                }
+              }}
             />
           </div>
         </div>
@@ -117,7 +126,7 @@ export default function MainPageBody() {
           <div className='mb-4'>
             <h2 className='text-xl font-bold'>많은 모임을 만들어낸 책</h2>
           </div>
-          <div className='grid grid-cols-1 md:grid-cols-5 gap-4 '>
+          <div className='grid grid-cols-1 md:grid-cols-5 gap-4 relative'>
             {popularBooks.map((book, idx) => (
               <div
                 key={idx}
@@ -129,8 +138,18 @@ export default function MainPageBody() {
                   backgroundRepeat: 'no-repeat',
                 }}
               >
-                <p className='text-xs text-white bg-gray-500 py-[8px] px-[8px] w-[auto] h-auto flex flex-row absolute right-2 bottom-2'>
-                  모집중인 모임
+                <div>
+                  <div className='absolute z-10 top-0 left-[26px] text-[22px] font-bold text-customGreen-500'>
+                    {idx + 1}
+                  </div>
+                  <BookRankIcon
+                    width={40}
+                    height={60}
+                    className='absolute -top-[6px] left-[12px]'
+                  />
+                </div>
+                <p className='text-[14px] text-white bg-[rgba(0,0,0,.5)] py-[2px] px-[6px] rounded-[2px] flex flex-row absolute right-2 bottom-2'>
+                  <span>모집중인 모임</span>
                   <span className='ml-2'>{book.gatheringCount}</span>
                 </p>
               </div>
@@ -139,11 +158,11 @@ export default function MainPageBody() {
         </div>
         <div className='w-[70rem]'>
           <div className='mb-4'>
-            <h2 className='text-xl font-bold mt-[82.13px]'>
+            <h2 className='text-2xl font-bold mt-[82.13px]'>
               어디서든 독서를 시작해요
             </h2>
           </div>
-          {/* FUNCTION PART */}
+          {/* FUNCTION PART - 목표 독서시간, 시작일 선택, 모집중만 보기, 정렬기능 */}
           <div className='flex flex-row items-center mb-2'>
             {(filterState.startDate || filterState.targetTime) && (
               <div
@@ -154,6 +173,19 @@ export default function MainPageBody() {
                 <RotateCwIcon width={24} height={24} />
               </div>
             )}
+
+            <div className='bg-gray-300 py-[5px] px-[10px] rounded-[30px] hover:bg-gray-400 duration-150 ml-2 flex flex-row gap-1 items-center font-bold'>
+              <CustomDropdown
+                trigger={
+                  filterState.targetTime
+                    ? filterState.targetTime
+                    : '목표 독서 시간'
+                }
+                items={['10분', '10분 ~ 30분', '30분 ~ 1시간', '1시간 이상']}
+                onSelect={(value) => handleFilterChange('targetTime', value)}
+              />
+              <ChevronDownIcon width={24} height={24} />
+            </div>
             <div className='bg-gray-300 py-[5px] px-[10px] rounded-[30px] hover:bg-gray-400 duration-150 ml-2 flex flex-row gap-2 items-center font-bold'>
               <CustomDropdown
                 trigger={
@@ -198,18 +230,6 @@ export default function MainPageBody() {
               </CustomDropdown>
               <CalendarIcon width={24} height={24} />
             </div>
-            <div className='bg-gray-300 py-[5px] px-[10px] rounded-[30px] hover:bg-gray-400 duration-150 ml-2 flex flex-row gap-1 items-center font-bold'>
-              <CustomDropdown
-                trigger={
-                  filterState.targetTime
-                    ? filterState.targetTime
-                    : '목표 독서 시간'
-                }
-                items={['10분', '10분 ~ 30분', '30분 ~ 1시간', '1시간 이상']}
-                onSelect={(value) => handleFilterChange('targetTime', value)}
-              />
-              <ChevronDownIcon width={24} height={24} />
-            </div>
             <div className='py-[5px] px-[10px] flex items-center ml-2 gap-2'>
               <Label htmlFor='recruiting-only'>모집중만 보기</Label>
               <Switch
@@ -221,48 +241,64 @@ export default function MainPageBody() {
               />
             </div>
           </div>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+          {/* MEETINGS PART - 모임목록 무한스크롤 */}
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             {filteredMeetings.map((meeting) => (
               <Link href={`/meeting-detail/${meeting.id}`} key={meeting.id}>
-                <div className='h-[298px] border rounded-lg shadow-md hover:shadow-lg bg-white flex flex-col'>
-                  <div className='w-full h-[148px] flex justify-center items-center bg-[#D9D9D9]'>
-                    <Image
-                      src={meeting.thumbnail}
-                      alt='meeting-thumbnail-image'
-                      width={360}
-                      height={148}
-                    />
-                  </div>
-                  <div className='flex flex-row p-4'>
-                    <div className='w-[87.8px] h-[132px] -mt-10 mr-3 flex justify-center items-center'>
+                <div className='w-full p-3 border rounded-lg shadow-md hover:shadow-lg  flex flex-col'>
+                  <div className='flex flex-row gap-4'>
+                    {/* MEETING IMAGE */}
+                    <div className='w-[120px] h-[180px] flex justify-center items-center'>
                       <Image
                         src={meeting.bookImage}
                         alt='meeting-bookcover-image'
-                        width={360}
-                        height={148}
+                        width={120}
+                        height={180}
                       />
                     </div>
-                    <div>
-                      <h3 className='text-lg font-semibold mb-3'>
-                        {meeting.name}
-                      </h3>
-                      <div className='flex flex-row gap-[2px]'>
-                        <UsersIcon width={16} height={16} />
-                        <p className='text-sm text-gray-600'>
-                          {meeting.currentCapacity}명 / {meeting.maxCapacity}명
-                        </p>
+                    <div className='flex flex-col w-[358px] justify-between'>
+                      <div className='flex flex-col'>
+                        <span className='bg-[#F3E7E7] text-customRed py-[2px] px-[6px] rounded-[2px] w-[103px] text-[14px]'>
+                          오늘부터 시작
+                        </span>
+                        <h3 className='text-lg font-bold mt-1'>
+                          {meeting.name}
+                        </h3>
                       </div>
-                      <div className='flex flex-row gap-[2px]'>
-                        <CalendarDotIcon width={16} height={16} />
-                        <p className='text-sm text-gray-600'>
-                          {meeting.gatheringWeek / 7}주 동안
-                        </p>
-                      </div>
-                      <div className='flex flex-row gap-[2px]'>
-                        <BookIcon width={24} height={18} />
-                        <p className='text-sm text-gray-600'>
-                          매일 {meeting.readingTimeGoal}분
-                        </p>
+                      {/* MEETING INFO */}
+                      <div>
+                        <div className='flex flex-row gap-[4px]'>
+                          <CalendarIcon width={20} height={20} />
+                          <p className='text-sm text-gray-600'>
+                            {meeting.gatheringWeek / 7}주 동안
+                          </p>
+                        </div>
+                        <div className='flex flex-row gap-[4px]'>
+                          <BookIcon width={20} height={20} />
+                          <p className='text-sm text-gray-600'>
+                            매일 {meeting.readingTimeGoal}분
+                          </p>
+                        </div>
+                        <div className='flex flex-row gap-[4px]'>
+                          <UsersIcon width={20} height={20} />
+                          <p className='text-sm text-gray-600'>
+                            {meeting.currentCapacity}명 /{' '}
+                            {meeting.maxCapacity < 1000
+                              ? meeting.maxCapacity
+                              : '∞'}
+                            명
+                          </p>
+                        </div>
+                        {/* PROGRESS BAR */}
+                        <div>
+                          <Progress
+                            className='mt-1'
+                            value={
+                              (meeting.currentCapacity / meeting.maxCapacity) *
+                              100
+                            }
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
